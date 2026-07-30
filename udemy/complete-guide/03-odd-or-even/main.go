883f9d729cf863313a74acf19abf658c7dcec29e
Gerd Reiss <gerd@reiss.pro>
2026-07-30 10:11:58 +0200

    Go: Complete Guide - exercise: odd or even improved

--- udemy/complete-guide/03-odd-or-even/main.go
+++ udemy/complete-guide/03-odd-or-even/main.go
@@ -6,11 +6,11 @@ func main() {
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, n := range numbers {
 		if n == 0 {
-			fmt.Println("zero")
+			fmt.Println(n, " is zero")
 		} else if n%2 == 0 {
-			fmt.Println("even")
+			fmt.Println(n, " is even")
 		} else {
-			fmt.Println("odd")
+			fmt.Println(n, " is odd")
 		}
 	}
 }
