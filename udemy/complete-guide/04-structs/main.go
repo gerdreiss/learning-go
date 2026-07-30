ebe1bc9f584070ba598aa1d87951bd8fb8991be7
Gerd Reiss <gerd@reiss.pro>
2026-07-30 19:32:46 +0200

    Go: Complete Guide - pointers

---
+++ udemy/complete-guide/04-structs/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p *person) updateFirstName(name string) {
+	p.firstName = name
+}
+
+func main() {
+	bond := person{
+		firstName: "James",
+		lastName:  "Bond",
+		contactInfo: contactInfo{
+			email: "james@bond.co.uk",
+			zip:   1000,
+		},
+	}
+	bond.print()
+	bond.updateFirstName("John")
+	bond.contactInfo.email = "john@bond.co.uk"
+	bond.print()
+}
