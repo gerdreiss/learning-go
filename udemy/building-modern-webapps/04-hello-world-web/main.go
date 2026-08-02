0cb50e7b1205135f61d349bd88dde26ea8e121cc
Gerd Reiss <gerd@reiss.pro>
2026-08-02 18:31:15 +0200

    Building Modern Web Applications with Go (Golang) - hello world web app

--- udemy/building-modern-webapps/04-hello-world-web/main.go
+++ udemy/building-modern-webapps/04-hello-world-web/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "Hello, world!")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Bytes written: %d\n\n", n)
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	res, err := divide(2, 2)
+	if err != nil {
+		fmt.Fprintf(w, "%s", err)
+	}
+	fmt.Fprintf(w, "This is the about page and 2/2 is %d\n", res)
+}
+
+func divide(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return x / y, nil
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Bytes written: %d\n\n", n)
-	},
-	)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
