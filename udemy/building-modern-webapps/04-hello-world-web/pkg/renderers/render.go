92ac9a52cccbf3b6d45bd3cac07703b228e599c4
Gerd Reiss <gerd@reiss.pro>
2026-08-05 20:38:05 +0200

    Building Modern Web Applications with Go (Golang) - cache the templates

--- udemy/building-modern-webapps/04-hello-world-web/pkg/renderers/render.go
+++ udemy/building-modern-webapps/04-hello-world-web/pkg/renderers/render.go
@@ -6,10 +6,35 @@ import (
 	"text/template"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsed, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsed.Execute(w, nil)
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	tmpl, exists := tc[t]
+	if !exists {
+		err := createTemplateCache(t)
+		if err != nil {
+			fmt.Println("shitty template: ", err)
+			return
+		}
+	}
+
+	err := tmpl.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
 	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	tc[t] = tmpl
+	return nil
 }
