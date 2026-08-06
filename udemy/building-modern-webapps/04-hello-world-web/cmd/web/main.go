311e5c72b99c97793de8082d0a07e44afa5f2ca9
Gerd Reiss <gerd@reiss.pro>
2026-08-06 15:04:41 +0200

    Building Modern Web Applications with Go (Golang) - use pat router

--- udemy/building-modern-webapps/04-hello-world-web/cmd/web/main.go
+++ udemy/building-modern-webapps/04-hello-world-web/cmd/web/main.go
@@ -8,30 +8,34 @@ import (
 	"github.com/gerdreiss/go-course/pkg/config"
 	"github.com/gerdreiss/go-course/pkg/handlers"
 	"github.com/gerdreiss/go-course/pkg/render"
-	"github.com/gerdreiss/go-course/pkg/utils"
 )
 
 const addr = ":8080"
 
 func main() {
-	var app config.AppConfig
-
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache: ", err)
 	}
 
-	app.UseCache = false
-	app.TemplateCache = tc
-	render.NewTemplates(&app)
+	app := config.AppConfig{
+		UseCache:      false,
+		TemplateCache: tc,
+		InfoLog:       log.Default(),
+	}
 
+	render.NewTemplates(&app)
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
-	http.HandleFunc("/divide/{x}/{y}", utils.Divide)
-
 	fmt.Printf("Starting server on port %s...\n", addr)
-	_ = http.ListenAndServe(addr, nil)
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
